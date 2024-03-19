c70dad7e5f468bacfb108a19fa0f8125f1e29ffa
Phạm Thành Lam <22520743@gm.uit.edu.vn>
2024-03-19 09:15:42 +0700

    Add ExtractEditText

---
+++ AndroidProject/app/src/main/java/com/example/extractedittext/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.extractedittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.inputmethodservice.ExtractEditText;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Tạo một ExtractEditText
+        ExtractEditText extractEditText = new ExtractEditText(this);
+        extractEditText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        extractEditText.setHint("Nhập văn bản");
+        extractEditText.requestFocus();
+
+        // Thêm thành phần vào layout
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        linearLayout.addView(extractEditText);
+
+        extractEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Hành động trước khi văn bản thay đổi
+                // Lấy văn bản hiện tại
+                String currentText = s.toString();
+                // Log văn bản hiện tại
+                Log.d("BeforeTextChanged", "Văn bản hiện tại: " + currentText);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Hành động khi văn bản đang được thay đổi
+                // Lấy văn bản mới
+                String newText = s.toString();
+
+                // Log văn bản mới
+                Log.d("OnTextChanged", "Văn bản mới: " + newText);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Lấy văn bản mới
+                String newText = s.toString();
+
+                Toast.makeText(MainActivity.this, "Văn bản mới: " + newText, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        extractEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    // Code khi có phím được nhấn
+                    Log.d("OnKey", "Đã có phím được nhấn.");
+                }
+                return false;
+            }
+        });
+    }
+
+
+}
