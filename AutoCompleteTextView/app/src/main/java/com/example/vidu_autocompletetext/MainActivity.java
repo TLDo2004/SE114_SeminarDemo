140dbcf9a4328847a4a3a2ac21fc59741f91ae61
Phạm Thành Lam <22520743@gm.uit.edu.vn>
2024-03-21 14:53:13 +0700

    add event

--- AutoCompleteTextView/app/src/main/java/com/example/vidu_autocompletetext/MainActivity.java
+++ AutoCompleteTextView/app/src/main/java/com/example/vidu_autocompletetext/MainActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private AutoCompleteTextView autoCompleteTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 arrDanToc);
         AutoCompleteTextView autoDanToc = findViewById(R.id.autoDanToc);
         autoDanToc.setAdapter(adapterDT);
+
+        autoCompleteTextView = findViewById(R.id.autoTinhThanh);
+        autoCompleteTextView.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    // Khi TextView nhận focus
+                    Log.e("OnFocusChangeListener",
+                            "Đã nhận focus trên autoCompleteTextView tỉnh thành!");
+                } else {
+                    // Khi TextView mất focus
+                    Log.e("OnFocusChangeListener",
+                            "Đã mất focus trên autoCompleteTextView tỉnh thành!");
+                }
+            }
+        });
+
+
+
+
     }
 }
--- EditText/app/src/main/java/com/example/vidu_edittext/MainActivity.java
+++ EditText/app/src/main/java/com/example/vidu_edittext/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.vidu_edittext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText ed1, ed2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,40 @@ public class MainActivity extends AppCompatActivity {
         String TK, MK;
         TK= edtNhapTK.getText().toString();
         MK= edtNhapMK.getText().toString();
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                edtNhapTK.setText("");
+                edtNhapMK.setText("");
+                // Ẩn bàn phím ảo
+                InputMethodManager imm =
+                        (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+
+        ed1 = findViewById(R.id.edt_NhapTK);
+        ed1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.e("OnClick","Nhap tai khoan.");
+            }
+        });
+
+        ed2 = findViewById(R.id.edt_NhapMK);
+        ed2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.e("OnClick","Nhap mat khau.");
+            }
+        });
+
+
+
+
     }
 }
